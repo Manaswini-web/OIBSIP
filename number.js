ebb98e0436e4c954c29159390850460311398deb
Manaswini-web <b22cs069@kitsw.ac.in>
2024-06-30 16:56:51 +0530

    Add files via upload
    calculator

---
+++ number.js
@@ -0,0 +1,22 @@
+// scripts.js
+const display = document.getElementById('display');
+
+function clearDisplay() {
+    display.textContent = '';
+}
+
+function deleteLast() {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+function appendToDisplay(value) {
+    display.textContent += value;
+}
+
+function calculate() {
+    try {
+        display.textContent = eval(display.textContent);
+    } catch {
+        display.textContent = 'Error';
+    }
+}
